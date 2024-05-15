99b3be562c3bbd9a095f345fabfdb5ba2849100f
Bernardo Leite <134572278+bernardoloures@users.noreply.github.com>
2024-05-15 18:53:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errorMessage = document.getElementById("error-message");
+
+    // Simulação de validação (substitua por sua lógica de autenticação real)
+    if (username === "losturcosfc@gmail.com" && password === "tropadolos123") {
+        // Exibir ícone de carregamento
+        document.getElementById("loading-icon").style.display = "inline";
+
+        setTimeout(function() {
+            window.location.href = "index.html"; //Redirecionar para a página do sistema de venda (index)
+        }, 3000); // 3000 milissegundos = 3 segundos
+        
+        // Limpar mensagem de erro e remover classe de erro dos campos
+        errorMessage.style.display = "none";
+        document.getElementById("username").classList.remove("error");
+        document.getElementById("password").classList.remove("error");
+    } else {
+        // Exibir mensagem de erro e adicionar classe de erro aos campos
+        errorMessage.style.display = "block";
+        document.getElementById("username").classList.add("error");
+        document.getElementById("password").classList.add("error");
+        
+        // Limpar campo de senha
+        document.getElementById("password").value = "";
+    }
+}
